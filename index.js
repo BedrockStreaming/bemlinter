35ddd502d7e6799c3cdeefcd2a2fd447a432e71e
Thomas ZILLIOX <thomas@zilliox.me>
2017-01-03 16:27:09 +0100

    Improve output styles

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const _ = require('lodash');
 const fs = require('mz/fs');
 const path = require('path');
+const colors = require('colors');
 const globby = require('globby');
 const {parse} = require('scss-parser');
 const minimist = require('minimist');
@@ -11,15 +12,12 @@ const createQueryAst = require('query-ast');
 
 let status = 0;
 
+// Utils
 const each = (wrapper, fn) => {
   for (let n of wrapper.nodes) { fn(n) }
 };
 
-function getBlockName(filePath) {
-  const fileName = path.basename(filePath);
-  return paramCase(fileName.slice(0, fileName.length - 4));
-}
-
+// Output
 function getContextualMessage(message, filePath, node) {
   const contextualMessage = [];
   if (filePath) {
@@ -43,6 +41,16 @@ function outputError(message, filePath, node) {
   status = 1;
 }
 
+function outputStatus(blockName, fileStatus) {
+  console.log(colors[fileStatus ? 'green' : 'red'](`  ${fileStatus ? '✓' : '✗'} ${blockName}`));
+}
+
+// BemLinter core
+function getBlockName(filePath) {
+  const fileName = path.basename(filePath);
+  return paramCase(fileName.slice(0, fileName.length - 4));
+}
+
 function isBlockWithAPseudoClass($wrapper) {
   return $wrapper.parent().next().get(0).type === 'pseudo_class';
 }
@@ -60,25 +68,31 @@ function isBlockNameOneOf(className, blockList, authorizedBlockName) {
 }
 
 function checkInternalClassName($, filePath, blockName) {
-  each($('class').find('identifier'), wrapper => {
+  return $('class').find('identifier').reduce((fileStatus, wrapper) => {
     const className = wrapper.node.value;
     if (!isBlockName(className, blockName)) {
       outputError(`".${className}" is incoherent with the file name.`, filePath, wrapper.node);
+      return false;
     }
-  });
+    
+    return fileStatus;
+  }, true);
 }
 
 function checkExternalClassName($, filePath, blockList, blockName) {
-  each($('class').find('identifier'), wrapper => {
+  return $('class').find('identifier').reduce((fileStatus, wrapper) => {
     const className = wrapper.node.value;
     if (isBlockNameOneOf(className, blockList, blockName)) {
       if (isBlockWithAPseudoClass($(wrapper))) {
         outputWarning(`".${className}" is tolerated in this stylesheet.`, filePath, wrapper.node);
       } else {
-        outputError(`".${className}" should not be style outside of his own stylesheet.`, filePath, wrapper.node); 
+        outputError(`".${className}" should not be style outside of his own stylesheet.`, filePath, wrapper.node);
+        return false;
       }
     }
-  });
+    
+    return fileStatus;
+  }, true);
 }
 
 function bemLintFile(filePath, blockList) {
@@ -87,12 +101,15 @@ function bemLintFile(filePath, blockList) {
       const ast = parse(data);
       const $ = createQueryAst(ast);
       const blockName = getBlockName(filePath);
+      let fileStatus;
+      
       if (blockList.indexOf(blockName) !== -1) {
-        console.log(filePath);
-        checkInternalClassName($, filePath, blockName);
+        fileStatus = checkInternalClassName($, filePath, blockName);
       } else {
-        checkExternalClassName($, filePath, blockList, blockName);
+        fileStatus = checkExternalClassName($, filePath, blockList, blockName);
       }
+      
+      outputStatus(blockName, fileStatus);
     })
     .catch(message => outputError(message, filePath))
   ;
@@ -115,6 +132,7 @@ const defaultConfig = {
   sources: argv._,
   ignore: []
 };
+
 new Promise(resolve => {
   if (!argv.config) {
     resolve(defaultConfig);
