85078377a8b60cdc4bcb7e2be3a742701d121a3e
Thomas ZILLIOX <thomas@zilliox.me>
2017-01-24 17:28:54 +0100

    Add a gulp-bemlinter plugin

---
+++ packages/gulp-bemlinter/__tests__/index.js
@@ -0,0 +1,15 @@
+const gulp = require('gulp');
+const bemlinter = require('./../src/gulp-bemlinter.js');
+
+describe('gulp-bemlinter', () => {
+  it('should lint', (done) => {
+    const spy = (output) => {
+      expect(output).toMatchSnapshot();
+      done();
+    };
+    
+    gulp.src('../bemlinter/__tests__/sources/*.scss')
+      .pipe(bemlinter())
+      .pipe(bemlinter.format(spy));
+  });
+});
